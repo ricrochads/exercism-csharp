44c3ed46d789e70a2cfc630e98763a5f7366a6da
Ricardo Rocha <ricrochads@outlook.com>
2023-10-27 00:10:25 -0300

    Reverse String

---
+++ ExercismCSharp/03.Strings/ReverseString.cs
@@ -0,0 +1,11 @@
+namespace ExercismCSharp;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
