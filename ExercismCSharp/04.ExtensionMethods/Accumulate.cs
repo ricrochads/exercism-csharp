d72b249bff5b9e354af5ae2856cf3335cd0e96d8
Ricardo Rocha <ricrochads@outlook.com>
2023-10-27 00:42:56 -0300

    Accumulate

---
+++ ExercismCSharp/04.ExtensionMethods/Accumulate.cs
@@ -0,0 +1,10 @@
+namespace ExercismCSharp;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+    {
+        foreach (var item in collection)
+            yield return func(item);
+    }
+}
